871060add0b649af1346d31a060b5527580ce3a3
Anatoly <anevmerzhitsky@mail.ru>
2018-05-10 10:39:47 +0300

    fives

--- main.cpp
+++ main.cpp
@@ -6,5 +6,12 @@ using namespace std;
 //Программа является учебной
 int main()
 {
-	
+		cout << "Введите первое число";
+	cin >> a;
+	cout << "Введите второе число";
+	cin >> b;
+	cout << "сумма:" << a+b;
+	cout << "разность:" << a-b;
+	return a;
+	return b;
 }
